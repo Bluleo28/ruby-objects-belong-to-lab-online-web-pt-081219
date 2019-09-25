029b9d100547b478431789ef6b54e5573284080e
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-09-25 02:20:39 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,16 +1,6 @@
 class Post
-  attr_accessor :author, :title
 
-  def initialize(title)
-    @title = title
-  end
+  attr_accessor :title, :author
 
-  def author_name
-    if author
-      self.author.name
-    else
-      nil
-    end
-  end
 
 end 
