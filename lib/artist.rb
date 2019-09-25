f4b6ea4ba10090ca85eabaedc35da3a7fccce489
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-09-25 01:45:26 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,11 +1,15 @@
 class Artist
   attr_accessor :name
 
-  @@all = []
+  @@song_count = 0
 
   def initialize(name)
     @name = name
     @songs= []
+  end
+
+  def songs
+    @songs
   end
 
   def add_song(song)
@@ -14,24 +18,15 @@ class Artist
     song
   end
 
-  def songs
-    @songs
+  def add_song_by_name(song_name)
+   song = Song.new(song_name)
+   @songs << song
+   song.artist = self
+   @@song_count += 1
+ end
+ 
+ def self.song_count
+  @@song_count
   end
 
-  def save
-    @@all << self
-    self
-  end
-
-  def self.all
-    @@all
-  end
-
-  def self.find_or_create_by_name(name)
-    self.all.detect {|artist| artist.name == name} || Artist.new(name).save
-  end
-
-  def print_songs
-    self.songs.each {|song| puts song.name}
-   end
 end
