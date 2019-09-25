3e32e515b4007be9f5e90979578d29b4934e4b8d
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-09-25 01:50:51 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,32 +1,27 @@
-class Artist
-  attr_accessor :name
+def initialize(name)
+  @name = name
+  @songs = [] #empty collection of songs
+end
 
-  @@song_count = 0
+def songs
+  @songs
+end
 
-  def initialize(name)
-    @name = name
-    @songs= []
-  end
+def add_song(song)
+  @songs << song
+  song.artist = self
+  @@song_count += 1
+end
 
-  def songs
-    @songs
-  end
+def add_song_by_name(song_name)
+  song = Song.new(song_name)
+  @songs << song
+  song.artist = self
+  @@song_count += 1
+end
 
-  def add_song(song)
-    @songs << song
-    song.artist = self
-    song
-  end
-
-  def add_song_by_name(song_name)
-   song = Song.new(song_name)
-   @songs << song
-   song.artist = self
-   @@song_count += 1
- end
-
- def self.song_count
+def self.song_count
   @@song_count
-  end
+end
 
 end
